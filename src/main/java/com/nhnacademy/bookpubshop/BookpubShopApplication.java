ca51ae1d4f1123028867c752954973ab4bf2f214
HoFe-U <hochul-y@naver.com>
2023-01-01 16:15:51 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpubShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/bookpubshop/BookpubShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpubShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
