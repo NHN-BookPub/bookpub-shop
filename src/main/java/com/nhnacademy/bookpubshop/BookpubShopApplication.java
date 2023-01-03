917df3b8d11927076e56e5a007db47c7c31fee22
seohyeon <kgustjgus@naver.com>
2023-01-03 21:27:51 +0900

    [Chore] jps 재 실행

--- src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
+++ src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookpubShopApplication {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(BookpubShopApplication.class, args);
 	}
