579be143cdbcc192c49c622eb468d347c0ce2858
seohyeon <kgustjgus@naver.com>
2023-01-04 17:12:06 +0900

    [Feat] PaymentTypeStateCode Entity 맵핑
    PaymentTypeStateCode Table Entity 맵핑 완료

---
+++ src/main/java/com/nhnacademy/bookpubshop/paymenttypestatecode/entity/PaymentTypeStateCode.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.bookpubshop.paymenttypestatecode.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Some description here.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "payment_type_state_code")
+public class PaymentTypeStateCode {
+    @Id
+    private Integer codeNumber;
+
+    @Column(name = "payment_type_state_code_name")
+    private String codeName;
+
+    @Column(name = "payment_type_state_code_used")
+    private boolean codeUsed;
+
+    @Column(name = "payment_type_state_code_info")
+    private String codeInfo;
+
+}
