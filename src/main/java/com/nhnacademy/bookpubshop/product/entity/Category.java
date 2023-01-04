82a7b220e5dd48518a961b8bb003e2aa75700c41
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-04 10:08:19 +0900

    [Feat] Category Entity 맵핑
    Category 테이블 엔티티 맵핑 완료

---
+++ src/main/java/com/nhnacademy/bookpubshop/product/entity/Category.java
@@ -0,0 +1,43 @@
+package com.nhnacademy.bookpubshop.product.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Some description here.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @Column(name = "category_number")
+    private Integer categoryNo;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "category_priority")
+    private Integer categoryPriority;
+
+    @Column(name = "category_displayed")
+    private boolean categoryDisplayed;
+
+    @ManyToOne
+    @JoinColumn(name = "category_number")
+    private Category parentNo;
+}
