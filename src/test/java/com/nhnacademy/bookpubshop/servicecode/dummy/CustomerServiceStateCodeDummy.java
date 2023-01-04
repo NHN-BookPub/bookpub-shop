1d86b937419237d2384fa46e0b2065f1bc369df7
HoFe-U <hochul-y@naver.com>
2023-01-05 01:47:14 +0900

    [Test] : CustomerServiceStateCode 기본 테스트구성
    dummy class 와 repo 기본 테스트추가
    
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/test/java/com/nhnacademy/bookpubshop/servicecode/dummy/CustomerServiceStateCodeDummy.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.bookpubshop.servicecode.dummy;
+
+import static com.nhnacademy.bookpubshop.state.CustomerServiceState.FAQ;
+import com.nhnacademy.bookpubshop.servicecode.entity.CustomerServiceStateCode;
+
+/**
+ * Some description here.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+public class CustomerServiceStateCodeDummy {
+
+    public static CustomerServiceStateCode dummy(){
+        return CustomerServiceStateCode
+                .builder()
+                .serviceCodeName(FAQ.getName())
+                .serviceCodeInfo(null)
+                .serviceCodeUsed(FAQ.isUsed())
+                .build();
+    }
+}
