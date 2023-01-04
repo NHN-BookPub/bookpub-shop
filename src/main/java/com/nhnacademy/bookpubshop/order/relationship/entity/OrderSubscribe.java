b2e7fbe905f5741c5cec85296c0f31449fe163d0
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-04 21:29:07 +0900

    [Refactor] OrderSubscribe 엔티티 클래스 수정
    OrderSubscribe 클래스 수정

--- src/main/java/com/nhnacademy/bookpubshop/order/relationship/entity/OrderSubscribe.java
+++ src/main/java/com/nhnacademy/bookpubshop/order/relationship/entity/OrderSubscribe.java
@@ -5,6 +5,8 @@ import com.nhnacademy.bookpubshop.subscribe.relationship.entity.OrderSubscribeSt
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -30,26 +32,29 @@ import lombok.ToString;
 @Table(name = "order_and_subscribe")
 public class OrderSubscribe {
     @Id
-    @Column(name = "order_subscribe_number")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_subscribe_number", nullable = false, unique = true)
     private Long subscribeNo;
 
     @ManyToOne
-    @JoinColumn(name = "subscribe_number")
+    @JoinColumn(name = "subscribe_number", nullable = false)
     private Subscribe subscribe;
 
+    // TODO 주문번호 Join
+
     @ManyToOne
-    @JoinColumn(name = "order_subscribe_state_code_number")
+    @JoinColumn(name = "order_subscribe_state_code_number", nullable = false)
     private OrderSubscribeStateCode orderSubscribeStateCode;
 
     @Column(name = "order_subscribe_coupon_amount")
     private Long couponAmount;
 
-    @Column(name = "order_subscribe_product_price")
+    @Column(name = "order_subscribe_product_price", nullable = false)
     private Long productPrice;
 
     @Column(name = "order_subscribe_finished_at")
     private LocalDateTime finishedAt;
 
-    @Column(name = "order_subscribe_created_at")
+    @Column(name = "order_subscribe_created_at", nullable = false)
     private LocalDateTime createdAt;
 }
