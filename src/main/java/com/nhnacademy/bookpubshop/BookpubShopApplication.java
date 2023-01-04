89f6463a051f0a185b1962843e6a5939bceca3f7
seohyeon <kgustjgus@naver.com>
2023-01-04 10:25:43 +0900

    [Feat] CouponType Entity 맵핑
    CouponType 테이블 엔티티 맵핑 완료

--- src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
+++ src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookpubShopApplication {
 
-	//
-	public static void main(String[] args) {
-		SpringApplication.run(BookpubShopApplication.class, args);
-	}
+    //
+    public static void main(String[] args) {
+        SpringApplication.run(BookpubShopApplication.class, args);
+    }
 
 }
