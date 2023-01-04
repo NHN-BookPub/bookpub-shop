b4d92ec748fea9791ab8b0a836bc7616bbb138a8
HoFe-U <hochul-y@naver.com>
2023-01-04 20:04:30 +0900

    [Refactor] : 구독State 값 수정 Co-authored-by: limtaewon <tagkdj1@gmail.com>
    구독상태에 수정 : 구독만료 추가
    
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/state/SubscribeState.java
+++ src/main/java/com/nhnacademy/bookpubshop/state/SubscribeState.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 
 /**
  * 구독상태를 알기위한 enum 입니다.
- * 종류는 구독중, 구독취소가 있습니다.
+ * 종류는 구독만료,구독중, 구독취소(환불)가 있습니다.
  *
  * @author : 유호철
  * @since : 1.0
  **/
 @Getter
 public enum SubscribeState {
+    COMPLETE_SUBSCRIPTION("구독만료", true),
     WAITING_SUBSCRIPTION("구독중", true),
     CANCEL_SUBSCRIPTION("구독취소", true);
 
