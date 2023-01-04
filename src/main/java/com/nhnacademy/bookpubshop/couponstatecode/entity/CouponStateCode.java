0695ec4dd97a04c6cfb4ef3f43b06f9f1b719e59
seohyeon <kgustjgus@naver.com>
2023-01-04 16:44:31 +0900

    [Feat] CouponStateCode Entity 맵핑
    CouponStateCode Entity 맵핑 완료

---
+++ src/main/java/com/nhnacademy/bookpubshop/couponstatecode/entity/CouponStateCode.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.bookpubshop.couponstatecode.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Some description here.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "coupon_state_code")
+public class CouponStateCode {
+
+    @Id
+    private Integer codeNumber;
+
+    @Column(name = "coupon_state_code_target")
+    private String codeTarget;
+
+    @Column(name = "coupon_state_code_used")
+    private boolean codeUsed;
+
+    @Column(name = "coupon_state_code_info")
+    private String codeInfo;
+
+}
