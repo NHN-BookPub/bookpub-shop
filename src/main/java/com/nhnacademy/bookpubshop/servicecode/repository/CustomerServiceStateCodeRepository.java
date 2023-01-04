69656f6f122777414b7e44eaacef0e4118eb8398
HoFe-U <hochul-y@naver.com>
2023-01-05 01:46:08 +0900

    [Feat] : CustomerServiceStateCodeRepo 추가
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/main/java/com/nhnacademy/bookpubshop/servicecode/repository/CustomerServiceStateCodeRepository.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.bookpubshop.servicecode.repository;
+
+import com.nhnacademy.bookpubshop.servicecode.entity.CustomerServiceStateCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 커스터머서비스 상태코드를 DB 와연결하기위한 Repo.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+public interface CustomerServiceStateCodeRepository
+        extends JpaRepository<CustomerServiceStateCode, Integer> {
+}
