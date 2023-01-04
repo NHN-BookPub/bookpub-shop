b0d4c893fffc80849ad59bc8e6473d00cce85909
seohyeon <kgustjgus@naver.com>
2023-01-04 19:33:33 +0900

    [Refactor] DeliveryLocation 엔티티 컬럼 추가
    DeliveryLocation 조인 컬럼 완료

--- src/main/java/com/nhnacademy/bookpubshop/deliverlocation/entity/DeliveryLocation.java
+++ src/main/java/com/nhnacademy/bookpubshop/deliverlocation/entity/DeliveryLocation.java
@@ -1,9 +1,12 @@
 package com.nhnacademy.bookpubshop.deliverlocation.entity;
 
+import com.nhnacademy.bookpubshop.delivery.entity.Delivery;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,9 +28,11 @@ public class DeliveryLocation {
 
     @Id
     @Column(name = "delivery_location_number")
-    private Long locationNum;
+    private Long locationNo;
 
-    //TODO : delivery_number join
+    @ManyToOne
+    @JoinColumn(name = "delivery_number")
+    private Delivery delivery;
 
     @Column(name = "delivery_location_name")
     private String locationName;
