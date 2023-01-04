8c9e4abd51622442dbc9aadfce9855afdd72fd00
HoFe-U <hochul-y@naver.com>
2023-01-05 02:44:00 +0900

    [Test] : CustomerService Test
    dummy 클래스 생성
    repo -> save 까지 테스트
    
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/test/java/com/nhnacademy/bookpubshop/service/dummy/CustomerServiceDummy.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.bookpubshop.service.dummy;
+
+import com.nhnacademy.bookpubshop.member.entity.Member;
+import com.nhnacademy.bookpubshop.service.entity.CustomerService;
+import com.nhnacademy.bookpubshop.servicecode.entity.CustomerServiceStateCode;
+import java.time.LocalDateTime;
+
+/**
+ * Some description here.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+public class CustomerServiceDummy {
+
+    public static CustomerService dummy(CustomerServiceStateCode customerServiceStateCode,
+                                        Member member){
+        return CustomerService
+                .builder()
+                .customerServiceStateCode(customerServiceStateCode)
+                .serviceCategory("category")
+                .serviceContent("content")
+                .serviceTitle("title")
+                .createdAt(LocalDateTime.now())
+                .member(member)
+                .build();
+    }
+}
