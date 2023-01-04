e9a35a639fcbce73c14d60071bbfd24ae98b0772
seohyeon <kgustjgus@naver.com>
2023-01-04 19:28:00 +0900

    [Feat] Delivery 엔티티 맵핑
    Delivery 엔티티 맵핑 완료

---
+++ src/main/java/com/nhnacademy/bookpubshop/delivery/entity/Delivery.java
@@ -0,0 +1,53 @@
+package com.nhnacademy.bookpubshop.delivery.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Some description here.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "delivery")
+public class Delivery {
+
+    @Id
+    @Column(name = "delivery_number")
+    private Long deliveryNumber;
+
+    //TODO : order_number join
+
+    @Column(name = "delivery_invoice_number")
+    private String invoiceNumber;
+
+    @Column(name = "delivery_company")
+    private String deliveryCompany;
+
+    @Column(name = "delivery_state")
+    private String deliveryState;
+
+    @Column(name = "delivery_request")
+    private String deliveryRequest;
+
+    @Column(name = "delivery_recipient")
+    private String deliveryRecipient;
+
+    @Column(name = "delivery_recipient_phone")
+    private String recipientPhone;
+
+    @Column(name = "delivery_finished_at")
+    private LocalDateTime finishedAt;
+
+}
