6ce474c49f59047bb84defe80fc71fb26ac692dd
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-04 21:29:21 +0900

    [Refactor] Product 엔티티 클래스 수정
    Product 클래스 수정

--- src/main/java/com/nhnacademy/bookpubshop/product/entity/Product.java
+++ src/main/java/com/nhnacademy/bookpubshop/product/entity/Product.java
@@ -1,9 +1,16 @@
 package com.nhnacademy.bookpubshop.product.entity;
 
+import com.nhnacademy.bookpubshop.product.relationship.entity.ProductPolicy;
+import com.nhnacademy.bookpubshop.product.relationship.entity.ProductSaleStateCode;
+import com.nhnacademy.bookpubshop.product.relationship.entity.ProductTypeStateCode;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -25,15 +32,27 @@ import lombok.ToString;
 @Entity
 @Table(name = "product")
 public class Product {
-
     @Id
-    @Column(name = "product_number")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_number", nullable = false, unique = true)
     private Long productNo;
 
-    @Column(name = "product_isbn")
+    @ManyToOne
+    @JoinColumn(name = "product_policy_number", nullable = false)
+    private ProductPolicy productPolicy;
+
+    @ManyToOne
+    @JoinColumn(name = "product_type_code_number", nullable = false)
+    private ProductTypeStateCode productTypeStateCode;
+
+    @ManyToOne
+    @JoinColumn(name = "product_sale_code_number", nullable = false)
+    private ProductSaleStateCode productSaleStateCode;
+
+    @Column(name = "product_isbn", nullable = false, unique = true)
     private String productIsbn;
 
-    @Column(name = "product_title")
+    @Column(name = "product_title", nullable = false)
     private String title;
 
     @Column(name = "product_page_count")
@@ -42,36 +61,36 @@ public class Product {
     @Column(name = "product_description")
     private String productDescription;
 
-    @Column(name = "prodcut_thumbnail")
+    @Column(name = "product_thumbnail")
     private String productThumbnail;
 
     @Column(name = "product_ebook_file_path")
     private String ebookFilePath;
 
-    @Column(name = "product_sales_price")
+    @Column(name = "product_sales_price", nullable = false)
     private Long salesPrice;
 
-    @Column(name = "product_sales_rate")
+    @Column(name = "product_sales_rate", nullable = false)
     private Integer salesRate;
 
-    @Column(name = "product_view_count")
+    @Column(name = "product_view_count", nullable = false)
     private Long viewCount;
 
-    @Column(name = "product_priority")
+    @Column(name = "product_priority", nullable = false)
     private Integer productPriority;
 
-    @Column(name = "product_deleted")
+    @Column(name = "product_deleted", nullable = false)
     private boolean productDeleted;
 
-    @Column(name = "product_stock")
+    @Column(name = "product_stock", nullable = false)
     private Integer productStock;
 
-    @Column(name = "product_publish_date")
+    @Column(name = "product_published_at", nullable = false)
     private LocalDateTime publishDate;
 
-    @Column(name = "product_created_at")
+    @Column(name = "product_created_at", nullable = false)
     private boolean createdAt;
 
-    @Column(name = "product_subscribed")
+    @Column(name = "product_subscribed", nullable = false)
     private boolean productSubscribed;
 }
