ad37e0aac991b3b81f833ccea69c2af3348e9b53
HoFe-U <hochul-y@naver.com>
2023-01-04 18:06:27 +0900

    [FEAT] : State 들 enum 등록 Co-authored-by: limtaewon <tagkdj1@gmail.com>
    쿠폰상태,고객서비스상태,문의상태,주문상품상태,주문상태,
    결제상태,주문종류상태,상품유형상태,구독상태를 정의
    
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/main/java/com/nhnacademy/bookpubshop/state/SubscribeState.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.bookpubshop.state;
+
+import lombok.Getter;
+
+/**
+ * 구독상태를 알기위한 enum 입니다.
+ * 종류는 구독중, 구독취소가 있습니다.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+@Getter
+public enum SubscribeState {
+    WAITING_SUBSCRIPTION("구독중", true),
+    CANCEL_SUBSCRIPTION("구독취소", true);
+
+    private final String name;
+    private final boolean isUsed;
+
+    SubscribeState(String name, boolean isUsed) {
+        this.name = name;
+        this.isUsed = isUsed;
+    }
+}
