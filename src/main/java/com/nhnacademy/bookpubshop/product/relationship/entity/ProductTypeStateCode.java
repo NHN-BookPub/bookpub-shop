23bce1a0e3cac611854daf672f71738094465f55
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-04 16:42:37 +0900

    [Feat] ProductTypeStateCode 엔티티 맵핑
    ProductTypeStateCode 엔티티 맵핑 완료

---
+++ src/main/java/com/nhnacademy/bookpubshop/product/relationship/entity/ProductTypeStateCode.java
@@ -0,0 +1,39 @@
+package com.nhnacademy.bookpubshop.product.relationship.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Some description here.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "product_type_state_code")
+public class ProductTypeStateCode {
+    @Id
+    @Column(name = "product_type_state_code_number")
+    private Integer codeNo;
+
+    @Column(name = "product_type_state_code_name")
+    private String codeName;
+
+    @Column(name = "product_type_state_code_used")
+    private boolean codeUsed;
+
+    @Column(name = "product_type_state_code_info")
+    private String codeInfo;
+}
