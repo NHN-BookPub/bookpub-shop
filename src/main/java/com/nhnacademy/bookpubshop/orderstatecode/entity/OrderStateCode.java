e1bbb2fc7cab09047061a01bc7ecf6f06b035cf2
seohyeon <kgustjgus@naver.com>
2023-01-04 18:12:43 +0900

    [Feat] OrderStateCode 엔티티 맵핑
    OrderStateCode 엔티티 맵핑 완료

---
+++ src/main/java/com/nhnacademy/bookpubshop/orderstatecode/entity/OrderStateCode.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.bookpubshop.orderstatecode.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Some description here.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "order_state_code")
+public class OrderStateCode {
+
+    @Id
+    @Column(name = "order_state_code_number")
+    private Integer codeNumber;
+
+    @Column(name = "order_state_code_name")
+    private String codeName;
+
+    @Column(name = "order_state_code_used")
+    private boolean codeUsed;
+
+    @Column(name = "order_state_code_info")
+    private String codeInfo;
+}
