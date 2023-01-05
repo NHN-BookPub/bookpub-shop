f669cab769c3194fdaeb69bb0dbd9fe61eb19650
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-05 14:45:03 +0900

    [Feat] Purchase 레포지토리 추가
    Purchase 테이블 레포지토리 추가

---
+++ src/main/java/com/nhnacademy/bookpubshop/purchase/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubshop.purchase.repository;
+
+import com.nhnacademy.bookpubshop.purchase.entity.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 매입이력(purchase) 레포지토리.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+}
