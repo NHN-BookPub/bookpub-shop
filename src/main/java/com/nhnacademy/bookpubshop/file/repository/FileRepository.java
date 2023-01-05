b43e6d500a7d7c3188bace7a3d5025564209676a
HoFe-U <hochul-y@naver.com>
2023-01-05 10:03:31 +0900

    [Docs] : java doc 빠진거 수정
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/file/repository/FileRepository.java
+++ src/main/java/com/nhnacademy/bookpubshop/file/repository/FileRepository.java
@@ -4,7 +4,7 @@ import com.nhnacademy.bookpubshop.file.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Some description here.
+ * File 테이블에 있는값을 가져오기위한 Repo 인터페이스입니다.
  *
  * @author : 유호철
  * @since : 1.0
