ff23c30423c56ee65e8f745348490b50abe2d9a4
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-05 14:57:50 +0900

    [Test] Member 테스트 더미
    Member 더미를 위한 테스트 코드

---
+++ src/test/java/com/nhnacademy/bookpubshop/member/dummy/MemberDummy.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.bookpubshop.member.dummy;
+
+import com.nhnacademy.bookpubshop.tier.dummy.TierDummy;
+import com.nhnacademy.bookpubshop.member.entity.Member;
+import com.nhnacademy.bookpubshop.tier.entity.Tier;
+import java.time.LocalDateTime;
+
+/**
+ * 회원 더미 클래스
+ * 테스트시 해당 클래스를 쉽게 생성하기 위한 클래스 입니다.
+ * 동료들과 협력하여 코딩할 때 유용합니다.
+ *
+ * @author : 임태원
+ * @since : 1.0
+ **/
+public class MemberDummy {
+    public static Member dummy(Tier tier) {
+        return new Member(null,
+                tier,
+                "id",
+                "nickname",
+                "taewon",
+                "남성",
+                1234,
+                1231,
+                "!@!#@ASD",
+                "12345678",
+                "email@email.com",
+                LocalDateTime.now(),
+                false,
+                false,
+                null,
+                0L,
+                false);
+    }
+}
