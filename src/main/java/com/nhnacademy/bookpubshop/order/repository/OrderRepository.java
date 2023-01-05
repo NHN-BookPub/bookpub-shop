daf99633c499fbbaddf272f9dec39fc531b21531
limtaewon <tagkdj1@gmail.com>
2023-01-05 10:21:32 +0900

    [Feat] OrderRepository 추가
    Order 레포지토리 추가

---
+++ src/main/java/com/nhnacademy/bookpubshop/order/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.bookpubshop.order.repository;
+
+import com.nhnacademy.bookpubshop.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 주문을 데이터베이스에서 다루기위한 Repo 클래스입니다.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
