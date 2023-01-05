7d1e746d65996b0a443c742f89b68314d953a81a
seohyeon <kgustjgus@naver.com>
2023-01-05 15:23:49 +0900

    [Test] PaymentRepositoryTest 추가
    PaymentRepository 테스트를 위한 더미 추가
    간단한 save 테스트

---
+++ src/test/java/com/nhnacademy/bookpubshop/payment/dummy/PaymentDummy.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.bookpubshop.payment.dummy;
+
+import com.nhnacademy.bookpubshop.order.entity.BookpubOrder;
+import com.nhnacademy.bookpubshop.payment.entity.Payment;
+import com.nhnacademy.bookpubshop.paymentstatecode.entity.PaymentStateCode;
+import com.nhnacademy.bookpubshop.paymenttypestatecode.entity.PaymentTypeStateCode;
+import java.time.LocalDateTime;
+
+/**
+ * 결제 더미클래스입니다.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+public class PaymentDummy {
+
+    public static Payment dummy(BookpubOrder order, PaymentStateCode paymentStateCode,
+            PaymentTypeStateCode paymentTypeStateCode) {
+        return new Payment(
+                null,
+                order,
+                paymentStateCode,
+                paymentTypeStateCode,
+                LocalDateTime.now()
+        );
+    }
+
+}
