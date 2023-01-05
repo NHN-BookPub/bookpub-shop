b365a76656e80355a171a57dd57e4351d043f8f0
seohyeon <kgustjgus@naver.com>
2023-01-05 09:36:54 +0900

    [Feat] PricePolicyRepository 추가
    PricePolicy 레포지토리 추가

---
+++ src/main/java/com/nhnacademy/bookpubshop/pricepolicy/repository/PricePolicyRepository.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.bookpubshop.pricepolicy.repository;
+
+import com.nhnacademy.bookpubshop.pricepolicy.entity.PricePolicy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 가격정책을 데이터베이스에서 다루기위한 Repo 클래스입니다.
+ *
+ * @author : 김서현
+ * @since : 1.0
+ **/
+public interface PricePolicyRepository extends JpaRepository<PricePolicy, Integer> {
+
+}
