eb8e2884ce7ef846cd89616f9edf9442f456c024
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-05 14:34:34 +0900

    [Refactor] 엔티티 수정
    Card 테이블 수정

--- src/main/java/com/nhnacademy/bookpubshop/card/entity/Card.java
+++ src/main/java/com/nhnacademy/bookpubshop/card/entity/Card.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
@@ -28,6 +29,10 @@ import lombok.NoArgsConstructor;
 public class Card {
 
     @Id
+    @Column(name = "payment_number")
+    private Long paymentNo;
+
+    @MapsId(value = "paymentNo")
     @OneToOne
     @JoinColumn(name = "payment_number", nullable = false)
     private Payment payment;
