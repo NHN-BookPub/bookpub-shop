5da244e032e87c48072cdce04ce81e4e396a0702
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-08 00:00:32 +0900

    [Feat] Tag Exception 구현
    Tag 관련 Exception 구현

---
+++ src/main/java/com/nhnacademy/bookpubshop/tag/exception/TagNameDuplicatedException.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.bookpubshop.tag.exception;
+
+/**
+ * 태그 이름 중복시 생기는 에러입니다.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+public class TagNameDuplicatedException extends RuntimeException {
+
+    private static final String ERROR_MESSAGE = " 태그 이름이 중복입니다.";
+
+    public TagNameDuplicatedException(String tagName) {
+        super(tagName + ERROR_MESSAGE);
+    }
+}
