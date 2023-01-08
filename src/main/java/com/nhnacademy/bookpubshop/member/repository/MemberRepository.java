66f931b80f44ae438524cb01f00e90de73270e35
limtaewon <tagkdj1@gmail.com>
2023-01-08 12:44:00 +0900

    [Refactor] 멤버중복조회 메소드 QueryDsl로 변경
    닉네임, 아이디, 이메일 중복검사를 위한 메소들 queryDsl로 재작성

--- src/main/java/com/nhnacademy/bookpubshop/member/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/bookpubshop/member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.bookpubshop.member.repository;
 
 import com.nhnacademy.bookpubshop.member.entity.Member;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -10,10 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author : 임태원
  * @since : 1.0
  **/
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByMemberNickname(String nickname);
+public interface MemberRepository extends JpaRepository<Member, Long>,
+        MemberRepositoryCustom {
 
-    Optional<Member> findByMemberId(String id);
-
-    Optional<Member> findByMemberEmail(String email);
 }
