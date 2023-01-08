8381f368905ab2af527824161d2b4f3c6c068589
limtaewon <tagkdj1@gmail.com>
2023-01-08 12:13:11 +0900

    [Refactor] 에러메시지 변수로 변경
    에러메시지 불변객체로 관리

--- src/main/java/com/nhnacademy/bookpubshop/member/exception/DuplicateMemberFieldException.java
+++ src/main/java/com/nhnacademy/bookpubshop/member/exception/DuplicateMemberFieldException.java
@@ -6,9 +6,11 @@ package com.nhnacademy.bookpubshop.member.exception;
  * @author : 임태원
  * @since : 1.0
  **/
-public class DuplicateMemberFieldException extends RuntimeException{
-    public DuplicateMemberFieldException(String message) {
-        super(message+"는 중복되는 항목입니다.");
+public class DuplicateMemberFieldException extends RuntimeException {
+    private static final String ERROR_MESSAGE = "는 중복되는 항목입니다.";
+
+    public DuplicateMemberFieldException(String field) {
+        super(field + ERROR_MESSAGE);
     }
 }
 
