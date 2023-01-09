520ed5f575c9598d82d6d40f52afeb8cb27a42a2
hobby3530 <jungy3530@gmail.com>
2023-01-10 00:21:47 +0900

    [Refactor] CouponType 조회 관련 Dto 수정
    GetCouponTypeResponseDto 에 typeNo 추가

--- src/main/java/com/nhnacademy/bookpubshop/coupontype/dto/response/GetCouponTypeResponseDto.java
+++ src/main/java/com/nhnacademy/bookpubshop/coupontype/dto/response/GetCouponTypeResponseDto.java
@@ -14,5 +14,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetCouponTypeResponseDto {
-    String typeName;
+    private Long typeNo;
+    private String typeName;
 }
