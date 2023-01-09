3a62b863dfdcbf9d31de085452ab362bcbdea053
woon <istana000@gmail.com>
2023-01-10 00:54:52 +0900

    [Feat] 상품 repository에서 쿼리 dsl을 사용하기 위한 상속 추가
    ProductRepositoryCustom 상속

--- src/main/java/com/nhnacademy/bookpubshop/product/repository/ProductRepository.java
+++ src/main/java/com/nhnacademy/bookpubshop/product/repository/ProductRepository.java
@@ -9,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author : 여운석
  * @since : 1.0
  **/
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 }
