e129ff22a4c472ada9185c96784bfd905809287b
woon <istana000@gmail.com>
2023-01-10 00:57:12 +0900

    [Feat] 상품판매상태코드 컨트롤러 생성
    상품판매상태코드 RestApi 구현.

---
+++ src/main/java/com/nhnacademy/bookpubshop/product/relationship/controller/ProductSaleStateCodeController.java
@@ -0,0 +1,91 @@
+package com.nhnacademy.bookpubshop.product.relationship.controller;
+
+import com.nhnacademy.bookpubshop.product.relationship.dto.CreateProductSaleStateCodeRequestDto;
+import com.nhnacademy.bookpubshop.product.relationship.dto.GetProductSaleStateCodeResponseDto;
+import com.nhnacademy.bookpubshop.product.relationship.service.ProductSaleStateCodeService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 상품판매정책코드에 관련된 컨트롤러입니다.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ */
+@RestController
+@RequestMapping("/api/state/productSale")
+public class ProductSaleStateCodeController {
+    private final ProductSaleStateCodeService productSaleStateCodeService;
+
+    /**
+     * 생성자입니다.
+     */
+    public ProductSaleStateCodeController(ProductSaleStateCodeService productSaleStateCodeService) {
+        this.productSaleStateCodeService = productSaleStateCodeService;
+    }
+
+    /**
+     * 모든 정책코드를 조회하는 api.
+     *
+     * @return 성공시 200과 모든 정책코드의 리스트가 반환됩니다.
+     */
+    @GetMapping
+    public ResponseEntity<List<GetProductSaleStateCodeResponseDto>> getAllSaleStateCode() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(productSaleStateCodeService.getAllProductSaleStateCode());
+    }
+
+    /**
+     * 상품판매정책코드를 생성하는 api.
+     *
+     * @param requestDto 생성에 사용하는 Dto.
+     * @return 성공시 201, 생성된 객체를 반환합니다.
+     */
+    @PostMapping
+    public ResponseEntity<GetProductSaleStateCodeResponseDto> createProductSaleStateCode(
+            @RequestBody CreateProductSaleStateCodeRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(productSaleStateCodeService.createSaleCode(requestDto));
+    }
+
+    /**
+     * 정책코드 번호로 조회하는 api.
+     *
+     * @param codeNo 정책코드번호입니다.
+     * @return 성공시 200, 객체를 반환합니다.
+     */
+    @GetMapping("/{codeNo}")
+    public ResponseEntity<GetProductSaleStateCodeResponseDto> getProductSaleStateCodeById(
+            @PathVariable Integer codeNo) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(productSaleStateCodeService.getSaleCodeById(codeNo));
+    }
+
+    /**
+     * 판매정책을 사용 여부를 수정하는 api
+     *
+     * @param codeNo 정책번호입니다.
+     * @param used   사용여부입니다.
+     * @return 성공시 200, 수정된 객체를 반환합니다.
+     */
+    @PostMapping("/{codeNo}")
+    public ResponseEntity<GetProductSaleStateCodeResponseDto> setUsedSaleCodeById(
+            @PathVariable Integer codeNo,
+            @RequestParam boolean used) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(productSaleStateCodeService.setUsedSaleCodeById(codeNo, used));
+    }
+}
