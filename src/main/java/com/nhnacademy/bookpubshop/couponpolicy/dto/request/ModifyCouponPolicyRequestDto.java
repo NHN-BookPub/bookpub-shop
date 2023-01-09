53e952e0cf029e20fca43381e0822827422db401
hobby3530 <jungy3530@gmail.com>
2023-01-09 12:24:34 +0900

    [Feat] CouponPolicy 관련 Dto 생성
    쿠폰정책 등록을 위한 CreateCouponPolicyRequestDto, 쿠폰정책 수정을 위한 ModifyCouponPolicyRequestDto, 쿠폰정책 조회를 위한 GetCouponPolicyResponseDto 생성

---
+++ src/main/java/com/nhnacademy/bookpubshop/couponpolicy/dto/request/ModifyCouponPolicyRequestDto.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.bookpubshop.couponpolicy.dto.request;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 쿠폰정책 수정을 위한 Dto.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+@Getter
+@NoArgsConstructor
+public class ModifyCouponPolicyRequestDto {
+    @NotNull(message = "정책번호를 기입하여야합니다.")
+    private Integer policyNo;
+
+    private boolean policyFixed;
+
+    @PositiveOrZero(message = "0 이상의 값을 기입하여야 합니다.")
+    private Long discountRate;
+
+    @PositiveOrZero(message = "0 이상의 값을 기입하여야 합니다.")
+    private Long policyMinimum;
+
+    @PositiveOrZero(message = "0 이상의 값을 기입하여야 합니다.")
+    private Long maxDiscount;
+}
