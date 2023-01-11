972adf0e53694fd85b122a68adf9f9774bea3540
limtaewon <tagkdj1@gmail.com>
2023-01-11 13:40:39 +0900

    [Feat] 로그인 기능 추가

---
+++ src/main/java/com/nhnacademy/bookpubshop/member/dto/response/LoginMemberResponseDto.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.bookpubshop.member.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 로그인 멤버의 반환 dto 클래스.
+ *
+ * @author : 임태원
+ * @since : 1.0
+ **/
+@Getter
+@AllArgsConstructor
+public class LoginMemberResponseDto {
+    private String memberId;
+    private List<String> authorities;
+}
