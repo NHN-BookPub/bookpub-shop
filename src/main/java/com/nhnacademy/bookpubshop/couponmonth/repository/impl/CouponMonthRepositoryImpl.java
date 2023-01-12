3742ff61e63e9ca5181c69780fc792f6f7900c09
hobby3530 <jungy3530@gmail.com>
2023-01-12 21:32:45 +0900

    [Refactor] CouponMonth 레포 수정

--- src/main/java/com/nhnacademy/bookpubshop/couponmonth/repository/impl/CouponMonthRepositoryImpl.java
+++ src/main/java/com/nhnacademy/bookpubshop/couponmonth/repository/impl/CouponMonthRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.nhnacademy.bookpubshop.couponmonth.entity.CouponMonth;
 import com.nhnacademy.bookpubshop.couponmonth.entity.QCouponMonth;
 import com.nhnacademy.bookpubshop.couponmonth.repository.CouponMonthRepositoryCustom;
 import com.nhnacademy.bookpubshop.coupontemplate.entity.QCouponTemplate;
+import com.nhnacademy.bookpubshop.file.entity.QFile;
 import com.querydsl.core.types.Projections;
 import java.util.List;
 import java.util.Optional;
@@ -29,15 +30,17 @@ public class CouponMonthRepositoryImpl extends QuerydslRepositorySupport
     public Optional<GetCouponMonthResponseDto> getCouponMonth(Long monthNo) {
         QCouponMonth couponMonth = QCouponMonth.couponMonth;
         QCouponTemplate couponTemplate = QCouponTemplate.couponTemplate;
+        QFile file = QFile.file;
 
         return Optional.of(from(couponMonth)
+                        .leftJoin(couponMonth.couponTemplate, file.couponTemplate)
                 .innerJoin(couponMonth.couponTemplate, couponTemplate)
                 .where(couponMonth.monthNo.eq(monthNo))
                 .select(Projections.constructor(GetCouponMonthResponseDto.class,
                         couponMonth.monthNo,
                         couponTemplate.templateNo,
                         couponTemplate.templateName,
-                        couponTemplate.templateImage,
+                        file.nameSaved.concat(file.fileExtension),
                         couponMonth.openedAt,
                         couponMonth.monthQuantity))
                 .fetchOne());
@@ -50,6 +53,7 @@ public class CouponMonthRepositoryImpl extends QuerydslRepositorySupport
     public List<GetCouponMonthResponseDto> getCouponMonths() {
         QCouponMonth couponMonth = QCouponMonth.couponMonth;
         QCouponTemplate couponTemplate = QCouponTemplate.couponTemplate;
+        QFile file = QFile.file;
 
         return from(couponMonth)
                 .innerJoin(couponMonth.couponTemplate, couponTemplate)
@@ -57,7 +61,7 @@ public class CouponMonthRepositoryImpl extends QuerydslRepositorySupport
                         couponMonth.monthNo,
                         couponTemplate.templateNo,
                         couponTemplate.templateName,
-                        couponTemplate.templateImage,
+                        file.nameSaved.concat(file.fileExtension),
                         couponMonth.openedAt,
                         couponMonth.monthQuantity))
                 .fetch();
