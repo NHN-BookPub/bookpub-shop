6167a5683b6ddff9a8024d029b1cced5a783fe56
hobby3530 <jungy3530@gmail.com>
2023-01-12 21:30:51 +0900

    [Feat] exception 추가
    CustomerService, PersonalInquiry, Review를 찾을 수 없을 경우의 exception 생성

---
+++ src/main/java/com/nhnacademy/bookpubshop/reviewpolicy/exception/ReviewNotFoundException.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.bookpubshop.reviewpolicy.exception;
+
+/**
+ * 리뷰를 찾을 수 없을 때 나는 exception.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public class ReviewNotFoundException extends RuntimeException {
+    private static final String MESSAGE = "번은 없는 리뷰 번호입니다.";
+
+    public ReviewNotFoundException(Long reviewNo) {
+        super(reviewNo + MESSAGE);
+    }
+}
