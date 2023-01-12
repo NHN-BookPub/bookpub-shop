c4d98ec440f0d62028a035308a8e215df55398f0
hobby3530 <jungy3530@gmail.com>
2023-01-12 14:24:44 +0900

    [Feat] image 를 다루는 FileUtils 생성
    쿠폰 이미지 처리를 위해 임시로 생성하였음

---
+++ src/main/java/com/nhnacademy/bookpubshop/utils/FileUtils.java
@@ -0,0 +1,56 @@
+package com.nhnacademy.bookpubshop.utils;
+
+import com.nhnacademy.bookpubshop.file.entity.File;
+import com.nhnacademy.bookpubshop.utils.exception.FileException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.UUID;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 파일을 읽어 처리하는 Util 클래스.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+public final class FileUtils {
+    private static final String BASE_PATH = "/static/image/";
+
+    private FileUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+
+    /**
+     * 파일을 저장하는 메소드입니다.
+     *
+     * @param name 카테고리명
+     * @param file 파일
+     * @return 저장 위치 반환
+     */
+    public static String saveFile(String name, MultipartFile file) {
+
+        String uuid = UUID.randomUUID().toString();
+        int pos = file.getName().indexOf(".");
+        String fileName = file.getName().substring(0, pos);
+        String fileExtension = file.getName().substring(pos);
+
+        try (
+                FileOutputStream fos = new FileOutputStream(BASE_PATH + uuid);
+                InputStream is = file.getInputStream()
+        ) {
+            int readCount;
+            byte[] buffer = new byte[1024];
+            while ((readCount = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, readCount);
+            }
+        } catch (Exception ex) {
+            throw new FileException();
+        }
+
+        new File(null, null, null, null, null, null,
+                name, BASE_PATH, fileExtension, fileName, uuid, null);
+
+        return BASE_PATH + uuid;
+    }
+}
