288df4968c4b37aef243ed81c539ff3a40b6ffe3
Woon <istana000@gmail.com>
2023-01-12 14:15:41 +0900

    [Refactor] 수정 Delete맵핑 -> Put맵핑
    url 맵핑 변경

--- src/main/java/com/nhnacademy/bookpubshop/product/relationship/controller/ProductSaleStateCodeController.java
+++ src/main/java/com/nhnacademy/bookpubshop/product/relationship/controller/ProductSaleStateCodeController.java
@@ -9,14 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 상품판매정책코드에 관련된 컨트롤러입니다.
@@ -78,7 +71,7 @@ public class ProductSaleStateCodeController {
      * @param used   사용여부입니다.
      * @return 성공시 201
      */
-    @DeleteMapping("/{codeNo}")
+    @PutMapping("/{codeNo}")
     public ResponseEntity<Void> setUsedSaleCodeById(
             @PathVariable Integer codeNo,
             @RequestParam boolean used) {
