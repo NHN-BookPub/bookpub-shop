e3221ce92739e7e96f1e756b822b2fe663550cc7
Woon <istana000@gmail.com>
2023-01-13 11:43:29 +0900

    [Feat] 매입이력 콘트롤러 제작
    매입이력 콘트롤러 제작

--- src/main/java/com/nhnacademy/bookpubshop/purchase/controller/PurchaseController.java
+++ src/main/java/com/nhnacademy/bookpubshop/purchase/controller/PurchaseController.java
@@ -1,20 +1,15 @@
 package com.nhnacademy.bookpubshop.purchase.controller;
 
-import com.nhnacademy.bookpubshop.purchase.dto.GetPurchaseResponseDto;
-import com.nhnacademy.bookpubshop.purchase.dto.SavePurchaseRequestDto;
+import com.nhnacademy.bookpubshop.purchase.dto.CreatePurchaseRequestDto;
+import com.nhnacademy.bookpubshop.purchase.dto.GetPurchaseListResponseDto;
 import com.nhnacademy.bookpubshop.purchase.service.PurchaseService;
+import com.nhnacademy.bookpubshop.utils.PageResponse;
 import lombok.RequiredArgsConstructor;
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 구매이력에 관련된 controller 입니다.
@@ -24,22 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/purchase")
+@RequestMapping("/api/purchases")
 public class PurchaseController {
     private final PurchaseService purchaseService;
+
+    /**
+     * 최신순으로 매입 이력을 반환합니다.
+     * 페이징.
+     *
+     * @return 200, 최신순으로 매입 이력을 반환합니다.
+     */
+    @GetMapping
+    public ResponseEntity<PageResponse<GetPurchaseListResponseDto>> getPurchaseListDesc(
+            Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(purchaseService.getPurchaseListDesc(pageable));
+    }
 
     /**
      * 상품번호로 구매이력을 조회하는 api.
      *
      * @param productNo 상품번호입니다.
-     * @return 구매이력 리스트가 반환됩니다.
+     * @return 200, 구매이력 리스트가 반환됩니다.
      */
     @GetMapping("/{productNo}")
-    public ResponseEntity<List<GetPurchaseResponseDto>> getPurchaseByProductNo(
-            @PathVariable Long productNo) {
+    public ResponseEntity<PageResponse<GetPurchaseListResponseDto>> getPurchaseByProductNo(
+            @PathVariable Long productNo,
+            Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(purchaseService.getPurchaseByProductNo(productNo));
+                .body(purchaseService.getPurchaseByProductNo(productNo, pageable));
     }
 
     /**
@@ -47,28 +57,45 @@ public class PurchaseController {
      *
      * @param purchaseNo  구매이력번호입니다.
      * @param request 수정시 사용되는 dto 입니다.
-     * @return 수정된 객체가 반환됩니다.
+     * @return 201 반환.
      */
     @PutMapping("/{purchaseNo}")
-    public ResponseEntity<GetPurchaseResponseDto> modifyPurchase(
+    public ResponseEntity<Void> modifyPurchase(
             @PathVariable Long purchaseNo,
-            @RequestBody SavePurchaseRequestDto request) {
-        return ResponseEntity.status(HttpStatus.OK)
+            @RequestBody CreatePurchaseRequestDto request) {
+        purchaseService.modifyPurchase(purchaseNo, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(purchaseService.modifyPurchase(purchaseNo, request));
+                .build();
     }
 
     /**
      * 구매이력을 생성하는 api.
      *
      * @param request 생성시 사용되는 dto class.
-     * @return 생성된 객체를 반환합니다.
+     * @return 201 반환.
      */
     @PostMapping
-    public ResponseEntity<GetPurchaseResponseDto> createPurchase(
-            @RequestBody SavePurchaseRequestDto request) {
+    public ResponseEntity<Void> createPurchase(
+            @RequestBody CreatePurchaseRequestDto request) {
+        purchaseService.createPurchase(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(purchaseService.createPurchase(request));
+                .build();
+    }
+
+    /**
+     * 매입이력 등록과 동시에 상품의 재고가 올라갑니다.
+     *
+     * @param request Dto 입니다.
+     * @return 201 반환.
+     */
+    @PostMapping("/absorption")
+    public ResponseEntity<Void> createPurchaseMerged(
+            @RequestBody CreatePurchaseRequestDto request) {
+        purchaseService.createPurchaseMerged(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
     }
 }
