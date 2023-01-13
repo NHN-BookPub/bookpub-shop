4e7d7b7fad08cdfb95cb24da2abec87e68e87bb9
Woon <istana000@gmail.com>
2023-01-13 11:44:19 +0900

    [Feat] 매입이력 레포지토리 커스텀 구현체 생성
    매입이력 레포지토리 커스텀 구현체 생성

--- src/main/java/com/nhnacademy/bookpubshop/purchase/repository/impl/PurchaseRepositoryImpl.java
+++ src/main/java/com/nhnacademy/bookpubshop/purchase/repository/impl/PurchaseRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.nhnacademy.bookpubshop.purchase.repository.impl;
 
 import com.nhnacademy.bookpubshop.product.entity.QProduct;
-import com.nhnacademy.bookpubshop.purchase.dto.GetPurchaseResponseDto;
+import com.nhnacademy.bookpubshop.purchase.dto.GetPurchaseListResponseDto;
 import com.nhnacademy.bookpubshop.purchase.entity.Purchase;
 import com.nhnacademy.bookpubshop.purchase.entity.QPurchase;
 import com.nhnacademy.bookpubshop.purchase.repository.PurchaseRepositoryCustom;
 import com.querydsl.core.types.Projections;
-import java.util.List;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
 
 /**
  * 매입 레포지토리의 구현체입니다.
@@ -15,30 +21,77 @@ import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
  * @author : 여운석
  * @since : 1.0
  **/
-public class PurchaseRepositoryImpl extends QuerydslRepositorySupport implements PurchaseRepositoryCustom {
-    public PurchaseRepositoryImpl() {
+public class PurchaseRepositoryImpl extends QuerydslRepositorySupport
+        implements PurchaseRepositoryCustom {
+    private final EntityManager entityManager;
+
+    public PurchaseRepositoryImpl(EntityManager entityManager) {
         super(Purchase.class);
+        this.entityManager = entityManager;
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public List<GetPurchaseResponseDto> findByProductNumber(Long number) {
+    public Page<GetPurchaseListResponseDto> findByProductNumberWithPage(
+            Long productNo, Pageable pageable) {
         QPurchase purchase = QPurchase.purchase;
         QProduct product = QProduct.product;
 
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
 
-        return from(purchase)
-                .innerJoin(product, purchase.product)
-                .where(product.productNo.eq(number))
+        JPAQuery<GetPurchaseListResponseDto> query = queryFactory
+                .from(purchase)
+                .join(purchase).on(product.productNo.eq(purchase.product.productNo))
+                .where(product.productNo.eq(productNo))
                 .select(Projections.constructor(
-                        GetPurchaseResponseDto.class,
+                        GetPurchaseListResponseDto.class,
+                        product.productNo,
                         purchase.purchaseNo,
-                        purchase.product.productNo,
-                        purchase.purchasePrice,
-                        purchase.createdAt,
-                        purchase.purchaseAmount))
-                .fetch();
+                        purchase.purchaseAmount,
+                        purchase.purchasePrice))
+                .orderBy(purchase.createdAt.desc())
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset());
+
+        Long count = queryFactory
+                .from(purchase)
+                .join(product).on(product.productNo.eq(purchase.product.productNo))
+                .select(purchase.count())
+                .fetchOne();
+
+        return new PageImpl<>(query.fetch(), pageable, count);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Page<GetPurchaseListResponseDto> getPurchaseListDesc(Pageable pageable) {
+        QPurchase purchase = QPurchase.purchase;
+        QProduct product = QProduct.product;
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+        JPAQuery<GetPurchaseListResponseDto> query = queryFactory
+                .from(purchase)
+                .join(product).on(product.productNo.eq(purchase.product.productNo))
+                .select(Projections.constructor(
+                        GetPurchaseListResponseDto.class,
+                        product.productNo,
+                        purchase.purchaseNo,
+                        purchase.purchaseAmount,
+                        purchase.purchasePrice))
+                .orderBy(purchase.createdAt.desc())
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset());
+
+        Long count = queryFactory
+                .select(purchase.count())
+                .from(purchase)
+                .fetchOne();
+
+        return new PageImpl<>(query.fetch(), pageable, count);
     }
 }
