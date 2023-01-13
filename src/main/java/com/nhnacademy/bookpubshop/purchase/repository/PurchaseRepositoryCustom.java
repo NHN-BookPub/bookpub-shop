2011ddfdb993948706b804e35612fee1c4ff0817
Woon <istana000@gmail.com>
2023-01-13 11:43:55 +0900

    [Feat] 매입이력 레포지토리 커스텀 생성
    매입이력 레포지토리 커스텀 생성

--- src/main/java/com/nhnacademy/bookpubshop/purchase/repository/PurchaseRepositoryCustom.java
+++ src/main/java/com/nhnacademy/bookpubshop/purchase/repository/PurchaseRepositoryCustom.java
@@ -1,7 +1,8 @@
 package com.nhnacademy.bookpubshop.purchase.repository;
 
-import com.nhnacademy.bookpubshop.purchase.dto.GetPurchaseResponseDto;
-import java.util.List;
+import com.nhnacademy.bookpubshop.purchase.dto.GetPurchaseListResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
@@ -12,7 +13,21 @@ import org.springframework.data.repository.NoRepositoryBean;
  **/
 @NoRepositoryBean
 public interface PurchaseRepositoryCustom {
-    List<GetPurchaseResponseDto> findByProductNumber(Long number);
+    /**
+     * 상품 번호로 매입이력을 조회합니다.
+     *
+     * @param productNo 상품번호.
+     * @param pageable 페이저블.
+     * @return 페이징된 구매이력리스트를 반환.
+     */
+    Page<GetPurchaseListResponseDto> findByProductNumberWithPage(
+            Long productNo, Pageable pageable);
 
-
+    /**
+     * 전체 매입이력을 조회합니다.
+     *
+     * @param pageable 페이징.
+     * @return 페이징된 구매이력리스트를 반환.
+     */
+    Page<GetPurchaseListResponseDto> getPurchaseListDesc(Pageable pageable);
 }
