373fd4c5e06e4f38ddc910893841f4beb15d39f7
Woon <istana000@gmail.com>
2023-01-13 11:39:42 +0900

    [Feat] 구매이력 조회시 사용하는 dto 추가
    구매이력 조회시 사용하는 dto 클래스

--- src/main/java/com/nhnacademy/bookpubshop/purchase/dto/GetPurchaseListResponseDto.java
+++ src/main/java/com/nhnacademy/bookpubshop/purchase/dto/GetPurchaseListResponseDto.java
@@ -2,20 +2,18 @@ package com.nhnacademy.bookpubshop.purchase.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import java.time.LocalDateTime;
 
 /**
- * 구매이력 조회시 반환되는 dto.
+ * 구매이력 조회시 사용되는 dto class.
  *
  * @author : 여운석
  * @since : 1.0
  **/
 @Getter
 @AllArgsConstructor
-public class GetPurchaseResponseDto {
-    private Long purchaseNo;
+public class GetPurchaseListResponseDto {
     private Long productNo;
+    private Long purchaseNo;
+    private Integer purchaseAmount;
     private Long purchasePrice;
-    private LocalDateTime createdAt;
-    private Long purchaseAmount;
 }
