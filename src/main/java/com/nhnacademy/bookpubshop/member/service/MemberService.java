45a53b6ccfa4681306d86b75f91bf7180fa4e663
HoFe-U <hochul-y@naver.com>
2023-01-14 20:51:44 +0900

    [Docs] : 자바 독 수정
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/member/service/MemberService.java
+++ src/main/java/com/nhnacademy/bookpubshop/member/service/MemberService.java
@@ -13,54 +13,57 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 /**
- * The interface Member service.
+ * 회원 레포지토리의 메소드를 이용하여 구현한 서버스입니다.
+ *
+ * @author : 임태원, 유호철
+ * @since : 1.0
  */
 public interface MemberService {
 
     /**
-     * Signup sign up member response dto.
+     * 회원생성시 필요한 메서드입니다.
      *
-     * @param signUpMemberRequestDto the sign up member request dto
-     * @return the sign up member response dto
+     * @param signUpMemberRequestDto 회원가입시 필요한 회원정보가 기입됩니다.
+     * @return signUpMemberResponseDto
      */
     SignUpMemberResponseDto signup(SignUpMemberRequestDto signUpMemberRequestDto);
 
     /**
-     * Modify member nick name.
+     * 회원 정보중 닉네임의 수정을 위해 필요한 메서드입니다.
      *
-     * @param memberNo   the member no
-     * @param requestDto the request dto
+     * @param memberNo   회원 번호.
+     * @param requestDto 수정할 회원의 닉네임이 기입된다.
      */
     void modifyMemberNickName(Long memberNo, ModifyMemberNicknameRequestDto requestDto);
 
     /**
-     * Modify member email.
+     * 회원 정보중 이메일을 수정하기위해 필요한 메서드입니다.
      *
-     * @param memberNo   the member no
-     * @param requestDto the request dto
+     * @param memberNo   회원 번호.
+     * @param requestDto 수정할 회원의 email 정보가 기입된다.
      */
     void modifyMemberEmail(Long memberNo, ModifyMemberEmailRequestDto requestDto);
 
     /**
-     * Gets member details.
+     * 회원에대한 상세정보가 반환됩니다.
      *
-     * @param memberNo the member no
-     * @return the member details
+     * @param memberNo 멤버번호.
+     * @return MemberDetailResponseDto 멤버에대한 상세값 반환.
      */
     MemberDetailResponseDto getMemberDetails(Long memberNo);
 
     /**
-     * Gets members.
+     * 멤버전체를 조회하기위한 메서드입니다.
      *
-     * @param pageable the pageable
-     * @return the members
+     * @param pageable 페이징 정보가 기입.
+     * @return page 페이징된 멤버정보가반환.
      */
     Page<MemberResponseDto> getMembers(Pageable pageable);
 
     /**
-     * Block member.
+     * 사용자를 차단 혹은 차단풀기 할때 사용되는 메서드입니다.
      *
-     * @param memberNo the member no
+     * @param memberNo 멤버번호반환.
      */
     void blockMember(Long memberNo);
 
@@ -70,7 +73,6 @@ public interface MemberService {
      * @param memberNo 멤버 번호기입.
      */
     void deleteMember(Long memberNo);
-
 
     /**
      * 멤버의 등급별 통계를 얻기위한 메서드입니다.
