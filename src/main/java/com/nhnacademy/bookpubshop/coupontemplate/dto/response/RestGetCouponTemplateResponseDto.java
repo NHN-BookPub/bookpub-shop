5074bd023d7122343fee54958d401029a9a6d374
hobby3530 <jungy3530@gmail.com>
2023-01-18 00:52:31 +0900

    [Feat] 파일 전송을 위한 쿠폰 Dto 생성
    front 서버로 파일 전송을 위한 dto 쿠폰 Dto 생성

---
+++ src/main/java/com/nhnacademy/bookpubshop/coupontemplate/dto/response/RestGetCouponTemplateResponseDto.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.bookpubshop.coupontemplate.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Some description here.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestGetCouponTemplateResponseDto {
+    private Long templateNo;
+    private String templateName;
+    private String templateImage;
+    private LocalDateTime issuedAt;
+    private LocalDateTime finishedAt;
+}
