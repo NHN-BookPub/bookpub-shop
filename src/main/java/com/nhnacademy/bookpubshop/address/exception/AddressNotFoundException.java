20a26d6a7710d0f0ffbb801372f555d65e335715
Woon <istana000@gmail.com>
2023-01-18 20:42:19 +0900

    [Feat] 주소 없음 예외 생성
    주소 없음 예외 생성

---
+++ src/main/java/com/nhnacademy/bookpubshop/address/exception/AddressNotFoundException.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.bookpubshop.address.exception;
+
+/**
+ * 주소가 존재하지 않을시 발생하는 예외.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+public class AddressNotFoundException extends RuntimeException {
+    private final static String message = "찾을 수 없는 주소입니다.";
+    public AddressNotFoundException() {
+        super(message);
+    }
+}
