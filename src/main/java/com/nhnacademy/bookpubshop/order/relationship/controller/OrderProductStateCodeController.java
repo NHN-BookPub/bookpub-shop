d6a07631a2e3e9e121c6aece32167ef632a6546d
Woon <istana000@gmail.com>
2023-01-18 23:00:28 +0900

    [Feat] 주문상품상태코드 컨트롤러 추가
    주문상품상태코드 컨트롤러 추가

---
+++ src/main/java/com/nhnacademy/bookpubshop/order/relationship/controller/OrderProductStateCodeController.java
@@ -0,0 +1,76 @@
+package com.nhnacademy.bookpubshop.order.relationship.controller;
+
+import com.nhnacademy.bookpubshop.order.relationship.dto.CreateOrderProductStateCodeRequestDto;
+import com.nhnacademy.bookpubshop.order.relationship.dto.GetOrderProductStateCodeResponseDto;
+import com.nhnacademy.bookpubshop.order.relationship.service.OrderProductStateCodeService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 주문상품상태코드 컨트롤러입니다.
+ *
+ * @author : 여운석
+ * @since : 1.0
+ **/
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/state/orderProduct")
+public class OrderProductStateCodeController {
+    private final OrderProductStateCodeService orderProductStateCodeService;
+
+    /**
+     * 모든 상태코드를 반환합니다.
+     *
+     * @return 200, 모든 상태코드 반환.
+     */
+    @GetMapping
+    public ResponseEntity<List<GetOrderProductStateCodeResponseDto>> getAllCodes() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(orderProductStateCodeService.getOrderProductStateCodes());
+    }
+
+    /**
+     * 코드 번호로 상태코드를 조회합니다.
+     *
+     * @param codeNo 코드번호.
+     * @return 200, 코드Dto 반환.
+     */
+    @GetMapping("/{codeNo}")
+    public ResponseEntity<GetOrderProductStateCodeResponseDto> getStateCodeById(
+            @PathVariable Integer codeNo) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(orderProductStateCodeService.getOrderProductStateCode(codeNo));
+    }
+
+    /**
+     * 상태코드를 등록합니다.
+     *
+     * @param request 코드 생성을 위한 Dto.
+     * @return 201 반환.
+     */
+    @PostMapping
+    public ResponseEntity<Void> createStateCode(@RequestBody CreateOrderProductStateCodeRequestDto request) {
+        orderProductStateCodeService.createOrderProductStateCode(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .build();
+    }
+
+    /**
+     * 상태코드를 수정합니다.
+     *
+     * @param codeNo 코드번호.
+     * @return 201 반환.
+     */
+    @PutMapping("/{codeNo}")
+    public ResponseEntity<Void> modifyStateCode(@PathVariable Integer codeNo) {
+        orderProductStateCodeService.modifyUsedOrderProductStateCode(codeNo);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .build();
+    }
+}
