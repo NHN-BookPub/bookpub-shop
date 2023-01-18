e9bdd3e29073e4a622bc8f849524ee68105407c0
Woon <istana000@gmail.com>
2023-01-18 20:44:03 +0900

    [Feat] Request, Response를 위한 Dto 클래스 추가
    Request, Response를 위한 Dto 클래스 추가

--- src/main/java/com/nhnacademy/bookpubshop/order/relationship/repository/OrderProductStateCodeRepository.java
+++ src/main/java/com/nhnacademy/bookpubshop/order/relationship/repository/OrderProductStateCodeRepository.java
@@ -1,6 +1,10 @@
 package com.nhnacademy.bookpubshop.order.relationship.repository;
 
 import com.nhnacademy.bookpubshop.order.relationship.entity.OrderProductStateCode;
+import com.nhnacademy.bookpubshop.order.relationship.repository.custom.OrderProductStateCodeRepositoryCustom;
+import com.nhnacademy.bookpubshop.state.OrderProductState;
+import com.nhnacademy.bookpubshop.state.anno.StateCode;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -10,5 +14,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since : 1.0
  **/
 public interface OrderProductStateCodeRepository
-        extends JpaRepository<OrderProductStateCode, Integer> {
+        extends JpaRepository<OrderProductStateCode, Integer>, OrderProductStateCodeRepositoryCustom {
+    /**
+     * 코드명으로 코드를 조회합니다.
+     *
+     * @param codeName 코드명입니다.
+     * @return
+     */
+    Optional<OrderProductStateCode> findByCodeName(
+            @StateCode(enumClass = OrderProductState.class)
+            String codeName);
 }
