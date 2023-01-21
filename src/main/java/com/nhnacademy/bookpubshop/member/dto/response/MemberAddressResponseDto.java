65f576bc0dfb8b2a381bafd7166726ec7695d536
HoFe-U <hochul-y@naver.com>
2023-01-21 18:39:55 +0900

    [Refactor] : 회원상세 주소에 주소값 첨가
    Signed-off-by: HoFe-U <hochul-y@naver.com>

---
+++ src/main/java/com/nhnacademy/bookpubshop/member/dto/response/MemberAddressResponseDto.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.bookpubshop.member.dto.response;
+
+import com.nhnacademy.bookpubshop.address.entity.Address;
+import lombok.Getter;
+
+/**
+ * 멤버에대한 주소정보를 반환하는 객체입니다.
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+@Getter
+public class MemberAddressResponseDto {
+    private final Long addressNo;
+    private final String roadAddress;
+    private final String addressDetail;
+    private final boolean addressBased;
+
+    /**
+     * 회원의 주소정보를 받기위한 생성자입니다.
+     *
+     * @param address the address
+     */
+    public MemberAddressResponseDto(Address address) {
+        this.addressNo = address.getAddressNo();
+        this.roadAddress = address.getRoadAddress();
+        this.addressDetail = address.getAddressDetail();
+        this.addressBased = address.isAddressMemberNumber();
+    }
+}
