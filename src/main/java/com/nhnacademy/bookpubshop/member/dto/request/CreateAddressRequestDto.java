58a9ec13c63457c8f37cdb341f23fc08af56a609
HoFe-U <hochul-y@naver.com>
2023-01-21 20:58:54 +0900

    [Refactor] : validation 추가
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/member/dto/request/CreateAddressRequestDto.java
+++ src/main/java/com/nhnacademy/bookpubshop/member/dto/request/CreateAddressRequestDto.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.bookpubshop.member.dto.request;
 
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateAddressRequestDto {
+    @NotNull(message = "주소값은 비어있을 수 없습니다.")
     private String address;
+    @NotNull(message = "상세주소는 비어있을 수 없습니다.")
     private String addressDetail;
 }
