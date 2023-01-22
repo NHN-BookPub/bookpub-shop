1133b56a9174f1c62fa2c5189663912c65d1ada0
limtaewon <tagkdj1@gmail.com>
2023-01-22 19:18:15 +0900

    [Feat] redis 관련 설정 추가
    token 을 디코딩하기 위한 redis 설정 추가
    추후 gateway로 이전작업 필요

--- src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
+++ src/main/java/com/nhnacademy/bookpubshop/BookpubShopApplication.java
@@ -2,7 +2,9 @@ package com.nhnacademy.bookpubshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Book-pub 시작.
@@ -15,6 +17,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * @author 박경서
  */
 @EnableJpaAuditing
+@EnableRedisHttpSession
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class BookpubShopApplication {
 
