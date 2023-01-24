745e5f435e2fba8e7db23b1ff158f10a5d90a3e7
HoFe-U <hochul-y@naver.com>
2023-01-24 21:01:33 +0900

    [Test] : 맡은 파트 rest docs 로 tc 변경
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/test/java/com/nhnacademy/bookpubshop/coupontype/controller/CouponTypeControllerTest.java
+++ src/test/java/com/nhnacademy/bookpubshop/coupontype/controller/CouponTypeControllerTest.java
@@ -5,6 +5,13 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,10 +22,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -29,6 +38,7 @@ import org.springframework.test.web.servlet.MockMvc;
  **/
 @WebMvcTest(CouponTypeController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 class CouponTypeControllerTest {
     @Autowired
     MockMvc mockMvc;
@@ -53,12 +63,20 @@ class CouponTypeControllerTest {
                 .willReturn(dto);
 
         //when && then
-        mockMvc.perform(get(path + "/{typeNo}", anyLong()))
+        mockMvc.perform(RestDocumentationRequestBuilders.get(path + "/{typeNo}", anyLong()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.typeNo", is(dto.getTypeNo()), Long.class))
-                .andExpect(jsonPath("$.typeName", equalTo(dto.getTypeName())));
-
+                .andExpect(jsonPath("$.typeName", equalTo(dto.getTypeName())))
+                .andDo(document("coupon-type-detail",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(parameterWithName("typeNo").description("Path 로 타입번호 기입")),
+                        responseFields(
+                                fieldWithPath("typeNo").description("쿠폰 유형 번호"),
+                                fieldWithPath("typeName").description("쿠폰 유형 명칭")
+                                )
+                ));
         verify(couponTypeService).getCouponType(anyLong());
     }
 
@@ -74,8 +92,15 @@ class CouponTypeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$[0].typeNo", is(dto.getTypeNo()), Long.class))
-                .andExpect(jsonPath("$[0].typeName", equalTo(dto.getTypeName())));
-
+                .andExpect(jsonPath("$[0].typeName", equalTo(dto.getTypeName())))
+                .andDo(document("coupon-type-list",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        responseFields(
+                                fieldWithPath("[].typeNo").description("쿠폰 유형 번호"),
+                                fieldWithPath("[].typeName").description("쿠폰 유형 명칭")
+                        )
+                ));
         verify(couponTypeService).getCouponTypes();
     }
 }
