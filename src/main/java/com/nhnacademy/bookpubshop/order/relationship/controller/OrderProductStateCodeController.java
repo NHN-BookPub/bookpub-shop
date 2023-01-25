2bf2649c48ddb480fbff78046b18ed9afbd675a2
seohyeon <kgustjgus@naver.com>
2023-01-26 00:32:57 +0900

    [Refactor] : validation 추가
    OrderProductStateCodeController @Valid 추가, CreateOrderProductStateCodeRequestDto validation 추가

--- src/main/java/com/nhnacademy/bookpubshop/order/relationship/controller/OrderProductStateCodeController.java
+++ src/main/java/com/nhnacademy/bookpubshop/order/relationship/controller/OrderProductStateCodeController.java
@@ -4,11 +4,19 @@ import com.nhnacademy.bookpubshop.order.relationship.dto.CreateOrderProductState
 import com.nhnacademy.bookpubshop.order.relationship.dto.GetOrderProductStateCodeResponseDto;
 import com.nhnacademy.bookpubshop.order.relationship.service.OrderProductStateCodeService;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 주문상품상태코드 컨트롤러입니다.
@@ -55,7 +63,8 @@ public class OrderProductStateCodeController {
      * @return 201 반환.
      */
     @PostMapping
-    public ResponseEntity<Void> createStateCode(@RequestBody CreateOrderProductStateCodeRequestDto request) {
+    public ResponseEntity<Void> createStateCode(
+            @RequestBody @Valid CreateOrderProductStateCodeRequestDto request) {
         orderProductStateCodeService.createOrderProductStateCode(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .build();
--- src/main/java/com/nhnacademy/bookpubshop/order/relationship/dto/CreateOrderProductStateCodeRequestDto.java
+++ src/main/java/com/nhnacademy/bookpubshop/order/relationship/dto/CreateOrderProductStateCodeRequestDto.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.bookpubshop.order.relationship.dto;
 
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * 주문상품상태코드를 위한 Dto.
@@ -12,7 +14,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CreateOrderProductStateCodeRequestDto {
+
+    @NotBlank(message = "주문 상품 상태 코드명은 필수 입력사항입니다.")
+    @Length(max = 20, message = "주문 상품 상태 코드명은 최대 20글자 가능합니다.")
     private String codeName;
     private boolean codeUsed;
+    @Length(max = 100, message = "정보는 최대 100글자 가능합니다.")
     private String codeInfo;
 }
