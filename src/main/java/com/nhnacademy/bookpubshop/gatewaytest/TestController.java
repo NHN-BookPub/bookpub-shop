bc08f68a3170307c3b39be1a8f3d978c03917f94
HoFe-U <hochul-y@naver.com>
2023-01-29 19:03:41 +0900

    [Refactor] : aop 테스트를 위한 controller 수정
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/gatewaytest/TestController.java
+++ src/main/java/com/nhnacademy/bookpubshop/gatewaytest/TestController.java
@@ -1,9 +1,11 @@
 package com.nhnacademy.bookpubshop.gatewaytest;
 
+import com.nhnacademy.bookpubshop.annotation.MemberAuth;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @Slf4j
 @RestController
-public class TestController
-{
-    @GetMapping("/test")
-    public String path( @RequestHeader HttpHeaders headers) {
+public class TestController {
+    @MemberAuth
+    @GetMapping("/test/{memberNo}")
+    public String path(
+            @PathVariable(value = "memberNo",required = false) Long memberNo,
+            @RequestHeader HttpHeaders headers) {
+        log.warn("memberNo : {}", memberNo);
         Map<String, String> map = headers.toSingleValueMap();
+
         log.warn(map.toString());
 
         return map.toString();
