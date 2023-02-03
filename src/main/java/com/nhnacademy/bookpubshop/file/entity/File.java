05a171eb9ed1ec3b53e40b07725b65009cbae01c
Woon <istana000@gmail.com>
2023-02-03 16:56:06 +0900

    [Refactor] fetch 전략 변경
    fetch 전략 변경

--- src/main/java/com/nhnacademy/bookpubshop/file/entity/File.java
+++ src/main/java/com/nhnacademy/bookpubshop/file/entity/File.java
@@ -6,15 +6,7 @@ import com.nhnacademy.bookpubshop.customersupport.entity.CustomerService;
 import com.nhnacademy.bookpubshop.personalinquiry.entity.PersonalInquiry;
 import com.nhnacademy.bookpubshop.product.entity.Product;
 import com.nhnacademy.bookpubshop.review.entity.Review;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -38,23 +30,23 @@ public class File extends BaseCreateTimeEntity {
     @Column(name = "file_number")
     private Long fileNo;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_number")
     private Review review;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "personal_inquiry_number")
     private PersonalInquiry personalInquiry;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "coupon_template_number")
     private CouponTemplate couponTemplate;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_number")
     private Product product;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "customer_service_number")
     private CustomerService customerService;
 
