e62f6a6452d0dd0887111dd58aee7a9816fb48b8
hobby3530 <jungy3530@gmail.com>
2023-02-05 16:01:45 +0900

    [Refactor] auth 관련 수정
    컨트롤러 매핑 주소 수정

--- src/main/java/com/nhnacademy/bookpubshop/author/controller/AuthorController.java
+++ src/main/java/com/nhnacademy/bookpubshop/author/controller/AuthorController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/authors")
+@RequestMapping
 public class AuthorController {
     private final AuthorService authorService;
 
