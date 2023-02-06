83947868a662680ed8677ffb2b9023c6bbe4ebea
hobby3530 <jungy3530@gmail.com>
2023-02-06 17:12:21 +0900

    [Docs] 체크스타일 적용 및 자바독 작성

--- src/main/java/com/nhnacademy/bookpubshop/reviewpolicy/repository/ReviewPolicyRepository.java
+++ src/main/java/com/nhnacademy/bookpubshop/reviewpolicy/repository/ReviewPolicyRepository.java
@@ -10,8 +10,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author : 임태원
  * @since : 1.0
  **/
-public interface ReviewPolicyRepository extends JpaRepository<ReviewPolicy, Integer>, ReviewPolicyRepositoryCustom {
+public interface ReviewPolicyRepository extends JpaRepository<ReviewPolicy, Integer>,
+        ReviewPolicyRepositoryCustom {
+    /**
+     * 상품평정책 사용여부가 참인 상품평 정책을 찾는 메서드입니다.
+     *
+     * @return the optional
+     */
     Optional<ReviewPolicy> findByPolicyUsedIsTrue();
 
+    /**
+     * 상품평정책 사용여부가 참인 상품평 정책이 있는지 확인하는 메서드입니다.
+     *
+     * @return the boolean
+     */
     boolean existsByPolicyUsedIsTrue();
 }
