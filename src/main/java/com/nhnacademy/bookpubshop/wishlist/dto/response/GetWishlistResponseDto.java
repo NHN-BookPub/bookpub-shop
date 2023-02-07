b468694650875f6a3ff172b732d216b362fa638e
Gyeongseo Bak <rudtj2966@gmail.com>
2023-02-07 22:35:15 +0900

    [Feat] 생성, 수정, 조회, 삭제 DTO 구현

---
+++ src/main/java/com/nhnacademy/bookpubshop/wishlist/dto/response/GetWishlistResponseDto.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.bookpubshop.wishlist.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 위시리스트 정보를 받기위한 Dto.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+@Getter
+@NoArgsConstructor
+public class GetWishlistResponseDto {
+    private Long productNo;
+    private String title;
+    private String productPublisher;
+    private String thumbnail;
+    private boolean wishlistApplied;
+}
