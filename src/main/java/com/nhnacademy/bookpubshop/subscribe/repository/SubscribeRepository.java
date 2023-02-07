312e0fd01e12369bc1c221efb46e3cc4d6506e54
HoFe-U <hochul-y@naver.com>
2023-02-07 20:06:10 +0900

    [HotFix] : 오류잡기 - 7
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/nhnacademy/bookpubshop/subscribe/repository/SubscribeRepository.java
@@ -10,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since : 1.0
  **/
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long>,
-        SubscribeRepositoryCustom {
+         SubscribeRepositoryCustom {
 }
