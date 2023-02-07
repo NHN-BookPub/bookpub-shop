36cbd8d82d2fd48770b5546b84b23848c80a065e
HoFe-U <hochul-y@naver.com>
2023-02-07 17:32:00 +0900

    [HotFix] : 오류잡기
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/file/entity/File.java
+++ src/main/java/com/nhnacademy/bookpubshop/file/entity/File.java
@@ -17,7 +17,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
 import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
