2220dd5dd64136a9c7d5597a26706f2fbcd050e8
Gyeongseo Bak <rudtj2966@gmail.com>
2023-02-07 22:36:12 +0900

    [Feat] 위시리스트 생성, 조회, 수정, 삭제 서비스 구현

---
+++ src/main/java/com/nhnacademy/bookpubshop/wishlist/service/WishlistService.java
@@ -0,0 +1,50 @@
+package com.nhnacademy.bookpubshop.wishlist.service;
+
+import com.nhnacademy.bookpubshop.wishlist.dto.request.CreateWishlistRequestDto;
+import com.nhnacademy.bookpubshop.wishlist.dto.request.DeleteWishlistRequestDto;
+import com.nhnacademy.bookpubshop.wishlist.dto.request.ModifyWishlistAlarmRequestDto;
+import com.nhnacademy.bookpubshop.wishlist.dto.response.GetWishlistResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * 위시리스트 서비스 인터페이스.
+ *
+ * @author : 박경서
+ * @since : 1.0
+ **/
+public interface WishlistService {
+
+    /**
+     * 위시리스트 생성 메서드.
+     *
+     * @param memberNo 멤버 번호
+     * @param request  생성을 위한 dto
+     */
+    void createWishlist(Long memberNo, CreateWishlistRequestDto request);
+
+    /**
+     * 멤버별 위시리스트를 조회하는 메서드.
+     *
+     * @param pageable 페이징 정보
+     * @param memberNo 멤버 번호
+     * @return 페이징 처리가 된 위시리스트 정보
+     */
+    Page<GetWishlistResponseDto> getWishlist(Pageable pageable, Long memberNo);
+
+    /**
+     * 위시리스트에서 상품을 삭제하는 메서드.
+     *
+     * @param memberNo 멤버 번호
+     * @param request  삭제를 위한 dto
+     */
+    void deleteWishlist(Long memberNo, DeleteWishlistRequestDto request);
+
+    /**
+     * 상품 알림 여부를 수정하는 메서드.
+     *
+     * @param memberNo 멤버 번호
+     * @param request  수정을 위한 dto
+     */
+    void modifyWishlistAlarm(Long memberNo, ModifyWishlistAlarmRequestDto request);
+}
