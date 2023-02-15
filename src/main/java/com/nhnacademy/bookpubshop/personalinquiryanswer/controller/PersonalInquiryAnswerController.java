659b8551dd62753e22c796b550c9fd4e2acc6fab
hobby3530 <jungy3530@gmail.com>
2023-02-15 20:39:46 +0900

    [Feat] 1대1문의답변 컨트롤러 생성 및 구현

---
+++ src/main/java/com/nhnacademy/bookpubshop/personalinquiryanswer/controller/PersonalInquiryAnswerController.java
@@ -0,0 +1,48 @@
+package com.nhnacademy.bookpubshop.personalinquiryanswer.controller;
+
+import com.nhnacademy.bookpubshop.annotation.AdminAuth;
+import com.nhnacademy.bookpubshop.personalinquiryanswer.dto.request.CreatePersonalInquiryAnswerRequestDto;
+import com.nhnacademy.bookpubshop.personalinquiryanswer.service.PersonalInquiryAnswerService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 1대1문의답변을 다루기 위한 컨트롤러입니다.
+ *
+ * @author : 정유진
+ * @since : 1.0
+ **/
+@RestController
+@RequiredArgsConstructor
+public class PersonalInquiryAnswerController {
+    private final PersonalInquiryAnswerService personalInquiryAnswerService;
+
+    /**
+     * 1대1문의답변을 생성하기 위한 메서드입니다.
+     *
+     * @param createDto 1대1문의답변 생성 시 필요한 정보를 담은 dto
+     */
+    @AdminAuth
+    @PostMapping("/token/personal-inquiry-answers")
+    public void personalInquiryAnswerAdd(
+            @Valid @RequestBody CreatePersonalInquiryAnswerRequestDto createDto) {
+        personalInquiryAnswerService.createPersonalInquiryAnswer(createDto);
+    }
+
+    /**
+     * 1대1문의답변을 삭제하기 위한 메서드입니다.
+     *
+     * @param answerNo 삭제할 1대1문의답변 번호
+     */
+    @AdminAuth
+    @DeleteMapping("/token/personal-inquiry-answers/{answerNo}")
+    public void personalInquiryAnswerDelete(
+            @PathVariable("answerNo") Long answerNo) {
+        personalInquiryAnswerService.deletePersonalInquiryAnswer(answerNo);
+    }
+}
