b46cac2f6a6c20dbff97f3884f433165f11b9214
HoFe-U <hochul-y@naver.com>
2023-02-26 16:29:35 +0900

    [Docs] : asciidoc 추가
    Signed-off-by: HoFe-U <hochul-y@naver.com>

--- src/main/java/com/nhnacademy/bookpubshop/address/entity/Address.java
+++ src/main/java/com/nhnacademy/bookpubshop/address/entity/Address.java
@@ -26,7 +26,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
